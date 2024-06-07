a9d0b24df815a0b387398828aa8a78ba918cd319
Hilario Petalbo, II <hilario@bluedrive.ph>
2024-06-07 17:18:55 +0000

    feat: add handle for different user authentication types

--- custom_logging/middleware.py
+++ custom_logging/middleware.py
@@ -1,17 +1,46 @@
-import threading
+from threading import local
 
-_thread_locals = threading.local()
+from django.utils.deprecation import MiddlewareMixin
+
+try:
+    from rest_framework.authentication import (
+        BasicAuthentication,
+        SessionAuthentication,
+        TokenAuthentication,
+    )
+except ImportError:
+    raise ImportError("Django Rest Framework is required to use this module")
+
+_thread_locals = local()
 
 
 def get_current_user():
     return getattr(_thread_locals, "user", None)
 
 
-class ModelLoggerMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+class CustomLoggingMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        _thread_locals.user = None
 
-    def __call__(self, request):
-        _thread_locals.user = request.user
-        response = self.get_response(request)
+        if request.user.is_authenticated:
+            _thread_locals.user = request.user
+        else:
+            user = None
+            for auth_class in [
+                BasicAuthentication,
+                SessionAuthentication,
+                TokenAuthentication,
+            ]:
+                auth_instance = auth_class()
+                try:
+                    user_auth_tuple = auth_instance.authenticate(request)
+                    if user_auth_tuple is not None:
+                        user, _ = user_auth_tuple
+                        break
+                except Exception:
+                    pass
+
+            _thread_locals.user = user
+
+    def process_response(self, request, response):
         return response
