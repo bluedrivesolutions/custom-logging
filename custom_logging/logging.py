8eab56568b43a22e48d5991ce92932bf7c2e3a3a
Hilario Petalbo, II <hilario@bluedrive.ph>
2024-06-09 18:08:47 +0000

    fix: fix overwriting save method on model

--- custom_logging/logging.py
+++ custom_logging/logging.py
@@ -1,4 +1,6 @@
 import logging
+import random
+import string
 
 from .middleware import get_current_user
 from .utils import convert_string_to_numeric
@@ -55,7 +57,8 @@ class LoggingMixin:
                 self.log_changes(old_instance)
             except self.__class__.DoesNotExist:
                 pass
-        super(self.__class__, self).save(*args, **kwargs)
+        save_method = getattr(self, self.original_save_method_name)
+        save_method(*args, **kwargs)
 
     def get_current_user(self):
         return get_current_user()
@@ -67,6 +70,12 @@ class ModelLogger:
         self.fields_to_log = fields_to_log
 
         original_init = model_class.__init__
+
+        original_save_method = model_class.save
+        original_save_method_name = "".join(
+            random.choices(string.ascii_uppercase + string.digits, k=10)
+        )
+        setattr(model_class, original_save_method_name, original_save_method)
 
         def __init__(self, *args, **kwargs):
             self.logging_fields = fields_to_log
@@ -82,7 +91,16 @@ class ModelLogger:
                 f"log_changes method is already defined in {model_class.__name__}"
             )
 
+        if hasattr(model_class, "original_save_method_name"):
+            logger.warning(
+                f"original_save_method_name method is already defined in {model_class.__name__}"
+            )
+            raise ValueError(
+                f"original_save_method_name method is already defined in {model_class.__name__}"
+            )
+
         model_class.__init__ = __init__
+        model_class.original_save_method_name = original_save_method_name
         model_class.save = LoggingMixin.save
         model_class.log_changes = LoggingMixin.log_changes
         model_class.get_current_user = LoggingMixin.get_current_user
