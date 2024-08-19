b21ebeea9a0dfc15f0c5101e12b38e7b224e87a2
Hilario Petalbo, II <hilario@bluedrive.ph>
2024-08-19 21:30:24 +0000

    refactor: use ipware to get client ip address

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 # setup.py
 from setuptools import find_packages, setup
 
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="custom-logging",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=requirements,
     author="Hilario Petalbo, II",
     author_email="hilario@bluedrive.ph",
     description="A simple Django model logger",
